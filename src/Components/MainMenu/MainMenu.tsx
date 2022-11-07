f2871ff20d2706d0a9fca4f398fb7048db1e1428
Sergey Litvinov <litvinows9@gmail.com>
2022-11-07 20:02:28 +0300

    project preparation

--- src/Components/MainMenu/MainMenu.tsx
+++ src/Components/MainMenu/MainMenu.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './MainMenu.css';
+
 
 function MainMenu() {
   return (
     <div className="main-menu">
-        <h1>Hi, i`m calculate</h1>
+        <h1 className='main-menu__title'>Hi, i`m calculate</h1>
+        <nav>
+          
+        </nav>
+
     </div>
   );
 }
