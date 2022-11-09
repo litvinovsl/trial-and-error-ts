56a10522b7b1defe7b030826a824e1983d5101ef
Sergey Litvinov <litvinows9@gmail.com>
2022-11-09 21:22:57 +0300

    created footer component

---
+++ src/Components/Footer/Footer.tsx
@@ -0,0 +1,14 @@
+import React, {FC} from 'react';
+import { Link } from 'react-router-dom';
+import './Footer.css';
+
+
+function Footer() {
+  return (
+    <div className="footer">
+         <p className='footer__copyright'>Litvinov Sergey.</p>
+     </div>
+  );
+}
+
+export default Footer;
