8efa067f7ea4820cc333fd9c36f25fadc52def77
Sergey Litvinov <litvinows9@gmail.com>
2022-11-11 20:49:09 +0300

    link to main page after click logo

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import logo from '../../images/logo.svg';
+import { Link } from 'react-router-dom';
 // import logo from '../../images/logo.svg';
 import './Header.css';
 
 function Header() {
+
+
+
   return (
-      <header className="header">
-        <img  className="header__logo" src={logo} alt="logo" />
-        <div></div>
-      </header>
+    <header className="header">
+      <Link to="/" >
+        <img className="header__logo" src={logo} alt="logo" />
+      </Link>
+      <div></div>
+    </header>
   );
 }
 
