84c9ca8740f6049887a02f5bc63e8c9a8ca114b8
Sergey Litvinov <litvinows9@gmail.com>
2022-11-11 21:17:33 +0300

    stylization of the form(in progress)

--- src/Components/CalculateForm/CalculateForm.tsx
+++ src/Components/CalculateForm/CalculateForm.tsx
@@ -3,9 +3,16 @@ import './CalculateForm.css';
 
 function CalculateForm() {
   return (
-    <div className="calculte__form">
-        тут
-    </div>
+    <section className="calculte">
+        <h1 className='calculte__title'>Расчет кредита</h1>
+        <form className='calculte__form'>
+            <input className='calculte__input-text' type='text' name='sum' />
+            <input className='calculte__input-text calculte__input-text_date' type='date' name='creditStartDate' />
+            <input className='calculte__input-text' type='text' name='interestRate' />
+            <input className='calculte__input-text' type='text' name='numberOfMonths' />
+            <input className='calculte__input-text' type='text' name='paymentDay' />
+        </form>
+    </section>
   );
 }
 
