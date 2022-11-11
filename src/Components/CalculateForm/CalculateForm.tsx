2da963d48f7917cc7be2c30ad77a5c5508b9fab4
Sergey Litvinov <litvinows9@gmail.com>
2022-11-12 01:06:09 +0300

    the calculate form is ready (it will be finalized in the future)

--- src/Components/CalculateForm/CalculateForm.tsx
+++ src/Components/CalculateForm/CalculateForm.tsx
@@ -2,18 +2,43 @@ import React from 'react';
 import './CalculateForm.css';
 
 function CalculateForm() {
-  return (
-    <section className="calculte">
-        <h1 className='calculte__title'>Расчет кредита</h1>
-        <form className='calculte__form'>
-            <input className='calculte__input-text' type='text' name='sum' />
-            <input className='calculte__input-text calculte__input-text_date' type='date' name='creditStartDate' />
-            <input className='calculte__input-text' type='text' name='interestRate' />
-            <input className='calculte__input-text' type='text' name='numberOfMonths' />
-            <input className='calculte__input-text' type='text' name='paymentDay' />
-        </form>
-    </section>
-  );
+    return (
+        <section className="calculte">
+            <h1 className='calculte__title'>Расчет кредита</h1>
+            <form className='calculte__form'>
+                <label className='calculate__label'>
+                    <input className='calculte__input' type='text' id='sum' name='sum' />
+                    <span className='calculte__span' 
+                    // htmlFor="sum"
+                    >Сумма кредита:</span>
+                </label>
+                {/* <label className='calculate__label'>
+                    <input className='calculte__input calculte__input_date' type='date' id='creditStartDate' name='creditStartDate' />
+                    <span className='calculte__span' 
+                    // htmlFor="creditStartDate"
+                    >Введите дату взятия кредита:</span>
+                </label> */}
+                <label className='calculate__label'>
+                    <input className='calculte__input' type='text' id='interestRate' name='interestRate' />
+                    <span className='calculte__span' 
+                    // htmlFor="interestRate"
+                    >Процентная ставка:</span>
+                </label>
+                <label className='calculate__label'>
+                    <input className='calculte__input' type='text' id='numberOfMonths' name='numberOfMonths' />
+                    <span className='calculte__span' 
+                    // htmlFor="numberOfMonths"
+                    >Колличество месяцев:</span>
+                </label>
+                <label className='calculate__label'>
+                    <input className='calculte__input' type='text' id='paymentDay' name='paymentDay' />
+                    <span className='calculte__span' 
+                    // htmlFor="paymentDay"
+                    >День платежа:</span>
+                </label>
+            </form>
+        </section>
+    );
 }
 
 export default CalculateForm;
