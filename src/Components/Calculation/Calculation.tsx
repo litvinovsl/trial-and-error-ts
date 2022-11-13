c9ba6ad9b1dbd7614eb2f2d5af3c8dbaee88f1a4
Sergey Litvinov <litvinows9@gmail.com>
2022-11-13 21:10:54 +0300

    create Calculation component

---
+++ src/Components/Calculation/Calculation.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+// import logo from '';
+// import './App.css';
+
+function Calculation() {
+  return (
+    <section className="calculation">
+        dd
+    </section>
+  );
+}
+
+export default Calculation;
