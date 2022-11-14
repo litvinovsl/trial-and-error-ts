b56a6df510c4b35828745c8445e8cf0d56cd89e2
Sergey Litvinov <litvinows9@gmail.com>
2022-11-14 21:32:46 +0300

    working with styles

--- src/Components/Calculation/Calculation.tsx
+++ src/Components/Calculation/Calculation.tsx
@@ -3,28 +3,24 @@ import React from 'react';
 import './Calculation.css';
 
 
-interface IDiagramStyle {
+interface ICalculationProps {
     interestPaid: number;
+    sum: number;
+    // creditStartDate: string;
+    // interestRate: number;
+    numberOfMonths: number;
+    // paymentDay: number;
 }
 
-function Calculation({interestPaid}: IDiagramStyle) {
+function Calculation(
+    {interestPaid, sum, numberOfMonths}: ICalculationProps) {
 
-    const data = {
-        sum: 100000,
-        creditStartDate: '26.04.2022',
-        interestRate: 10,
-        numberOfMonths: 24,
-        paymentDay: 15,
-        percentages: 95,
-    }
-
-    // p = 25
 
     return (
         <section className="calculation">
             <div className='calculation__credit-info'>
                 <div className='calculation__credit-info-text'>
-                    <p className='calculation__credit-paragraph'>Кредит на сумму 100 000,00 ₽, 24 месяцев.</p>
+                    <p className='calculation__credit-paragraph'>Кредит на сумму {sum} ₽, {numberOfMonths} месяцев.</p>
                     <p className='calculation__credit-paragraph'>На текущий момент остаток долга 2 109,36 ₽.</p>
                     <p className='calculation__credit-paragraph'>Выплачен долг 97 890,64 ₽, проценты 2 447,81 ₽.</p>
                     <p className='calculation__credit-paragraph'>Всего выплачено процентов 2 475,68 ₽.</p>
@@ -35,7 +31,7 @@ function Calculation({interestPaid}: IDiagramStyle) {
                 </div>
                 <div className='calculation__diagram'>
                     <div className="pie animate" style={{['--p' as any] : interestPaid }}> {interestPaid}%</div>
-                    <p className='calculation__diagram'>*выплачено</p>
+                    <p className='calculation__diagram-paragraph'>*выплачено</p>
                 </div>
             </div>
         </section>
