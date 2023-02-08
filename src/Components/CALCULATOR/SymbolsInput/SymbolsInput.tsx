e44211992d174b6186d11bc3b1b4c43985971996
Sergey Litvinov <litvinows9@gmail.com>
2023-02-08 19:56:04 +0300

    working on the functionality of the calculator

--- src/Components/CALCULATOR/SymbolsInput/SymbolsInput.tsx
+++ src/Components/CALCULATOR/SymbolsInput/SymbolsInput.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './SymbolsInput.css';
 
 function SymbolsInput({result, setResult}:any){
@@ -18,22 +18,48 @@ function SymbolsInput({result, setResult}:any){
     // const [minus, setMinus] = useState(false);
     // const [plus, setPlus] = useState(false);
     // const [negative, setNegative] = useState(false);
-    // const [equal, setEqual] = useState(false);
+    const [active, setActive] = useState(true);
+    useEffect(() => {
 
+    }, [])
     function handleSubtraction(){
       // handleResult();
       // if (result.includes('+') || result.includes('-') || result.includes('*') || result.includes('/')) {
       //   console.log('first', result)
 
       //   handleResult();
-
-        setResult(result + '-');
+      
+        // setResult(result + '-');
       //   console.log('include', result)
       // } else {
       //   setResult(result + '-');
       //   console.log('hernya')
       // }
+
+      if (active){
+        setResult(result + '-');
+        setActive(false);
+      }
     }
+    function handleSumm(){
+      if (active){
+        setResult(result + '+');
+        setActive(false);
+      }
+    }
+    function handleMultiplications(){
+      if (active){
+        setResult(result + '*');
+        setActive(false);
+      }
+    }
+    function handleSplitting(){
+      if (active){
+        setResult(result + '/');
+        setActive(false);
+      }
+    }
+
     function handleResult(){
       let sum;
 
@@ -43,18 +69,22 @@ function SymbolsInput({result, setResult}:any){
         setResult(String(sum));
         console.log('plus-res', result)
         console.log('plus-sum', result)
+        setActive(true);
       } else if (result.includes('-')) {
         sum = result.split('-');
         sum = parseInt(sum[0]) - parseInt(sum[1]);
         setResult(String(sum));
+        setActive(true);
       } else if (result.includes('*')) {
         sum = result.split('*');
         sum = parseInt(sum[0]) * parseInt(sum[1]);
         setResult(String(sum));
+        setActive(true);
       } else if (result.includes('/')) {
         sum = result.split('/');
         sum = parseInt(sum[0]) / parseInt(sum[1]);
         setResult(String(sum));
+        setActive(true);
       } else {
         setResult(result)
       }
@@ -67,22 +97,22 @@ function SymbolsInput({result, setResult}:any){
   // console.log(-result)
   return (
     <div className='container'>
-        <div className='conteiner__symbol' onClick={() => setResult('')}>AC</div>
+        <div className='conteiner__symbol' onClick={() => {setActive(true) ;setResult('')}}>AC</div>
         <div className='conteiner__symbol' onClick={() => setResult(-result)}>+/-</div>
         <div className='conteiner__symbol'>%</div>
-        <div className='conteiner__symbol' onClick={() => setResult(result + '/')}>/</div>
+        <div className={active ? `conteiner__symbol` : `conteiner__symbol_off`} onClick={handleSplitting}>/</div>
         <div className='conteiner__symbol' onClick={() => {setResult(result + '7')}}>7</div>
         <div className='conteiner__symbol' onClick={() => setResult(result + '8')}>8</div>
         <div className='conteiner__symbol' onClick={() => setResult(result + '9')}>9</div>
-        <div className='conteiner__symbol' onClick={() => setResult(result + '*')}>*</div>
+        <div className={active ? `conteiner__symbol` : `conteiner__symbol_off`} onClick={handleMultiplications}>*</div>
         <div className='conteiner__symbol' onClick={() => setResult(result + '4')}>4</div>
         <div className='conteiner__symbol' onClick={() => setResult(result + '5')}>5</div>
         <div className='conteiner__symbol' onClick={() => setResult(result + '6')}>6</div>
-        <div className='conteiner__symbol' onClick={() => handleSubtraction()}>-</div>
+        <div className={active ? `conteiner__symbol` : `conteiner__symbol_off`} onClick={handleSubtraction}>-</div>
         <div className='conteiner__symbol' onClick={() => setResult(result + '1')}>1</div>
         <div className='conteiner__symbol' onClick={() => setResult(result + '2')}>2</div>
         <div className='conteiner__symbol' onClick={() => setResult(result + '3')}>3</div>
-        <div className='conteiner__symbol' onClick={() => setResult(result + '+')}>+</div>
+        <div className={active ? `conteiner__symbol` : `conteiner__symbol_off`} onClick={handleSumm}>+</div>
         <div className='conteiner__symbol conteiner__symbol_0' onClick={() => setResult(result + '0')}>0</div>
         <div className='conteiner__symbol'>,</div>
         <div className='conteiner__symbol' onClick={handleResult}>=</div>
